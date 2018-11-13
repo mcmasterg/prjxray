14428320376d8f5b28d778e0efd1d39fd1b077f1
John McMaster <johndmcmaster@gmail.com>
2018-11-13 14:53:06 -0800

    intpips generate.py: break into functions
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- fuzzers/050-intpips/generate.py
+++ fuzzers/050-intpips/generate.py
@@ -4,57 +4,69 @@ import re
 
 from prjxray.segmaker import Segmaker
 
-segmk = Segmaker("design.bits")
+def load():
+    '''
+    tiledata: info on pips in each tile
+    ignpip: set of pseudo pips. Don't emit symbols for these
+    pipdata: pipdata[pip name] = (src pip, dst pip)
+    '''
 
-tiledata = dict()
-pipdata = dict()
-ignpip = set()
+    tiledata = dict()
+    pipdata = dict()
+    ignpip = set()
 
-print("Loading tags from design.txt.")
-with open("design.txt", "r") as f:
-    for line in f:
-        tile, pip, src, dst, pnum, pdir = line.split()
-        _, pip = pip.split(".")
-        _, src = src.split("/")
-        _, dst = dst.split("/")
-        pnum = int(pnum)
-        pdir = int(pdir)
+    print("Loading tags from design.txt.")
+    with open("design.txt", "r") as f:
+        for line in f:
+            tile, pip, src, dst, pnum, pdir = line.split()
+            _, pip = pip.split(".")
+            _, src = src.split("/")
+            _, dst = dst.split("/")
+            pnum = int(pnum)
+            pdir = int(pdir)
 
-        if tile not in tiledata:
-            tiledata[tile] = {"pips": set(), "srcs": set(), "dsts": set()}
+            if tile not in tiledata:
+                tiledata[tile] = {"pips": set(), "srcs": set(), "dsts": set()}
 
-        if pip in pipdata:
-            assert pipdata[pip] == (src, dst)
-        else:
-            pipdata[pip] = (src, dst)
+            if pip in pipdata:
+                assert pipdata[pip] == (src, dst)
+            else:
+                pipdata[pip] = (src, dst)
 
-        tiledata[tile]["pips"].add(pip)
-        tiledata[tile]["srcs"].add(src)
-        tiledata[tile]["dsts"].add(dst)
+            tiledata[tile]["pips"].add(pip)
+            tiledata[tile]["srcs"].add(src)
+            tiledata[tile]["dsts"].add(dst)
 
-        if pdir == 0:
-            tiledata[tile]["srcs"].add(dst)
-            tiledata[tile]["dsts"].add(src)
+            if pdir == 0:
+                tiledata[tile]["srcs"].add(dst)
+                tiledata[tile]["dsts"].add(src)
 
-        if pnum == 1 or pdir == 0 or \
-                re.match(r"^(L[HV]B?|G?CLK)(_L)?(_B)?[0-9]", src) or \
-                re.match(r"^(L[HV]B?|G?CLK)(_L)?(_B)?[0-9]", dst) or \
-                re.match(r"^(CTRL|GFAN)(_L)?[0-9]", dst):
-            ignpip.add(pip)
+            if pnum == 1 or pdir == 0 or \
+                    re.match(r"^(L[HV]B?|G?CLK)(_L)?(_B)?[0-9]", src) or \
+                    re.match(r"^(L[HV]B?|G?CLK)(_L)?(_B)?[0-9]", dst) or \
+                    re.match(r"^(CTRL|GFAN)(_L)?[0-9]", dst):
+                ignpip.add(pip)
 
-for tile, pips_srcs_dsts in tiledata.items():
-    pips = pips_srcs_dsts["pips"]
-    srcs = pips_srcs_dsts["srcs"]
-    dsts = pips_srcs_dsts["dsts"]
+    return tiledata, pipdata, ignpip
 
-    for pip, src_dst in pipdata.items():
-        src, dst = src_dst
-        if pip in ignpip:
-            pass
-        elif pip in pips:
-            segmk.add_tile_tag(tile, "%s.%s" % (dst, src), 1)
-        elif src_dst[1] not in dsts:
-            segmk.add_tile_tag(tile, "%s.%s" % (dst, src), 0)
+def run():
+    tiledata, pipdata, ignpip = load()
 
-segmk.compile()
-segmk.write()
+    segmk = Segmaker("design.bits")
+
+    for tile, pips_srcs_dsts in tiledata.items():
+        pips = pips_srcs_dsts["pips"]
+        srcs = pips_srcs_dsts["srcs"]
+        dsts = pips_srcs_dsts["dsts"]
+
+        for pip, src_dst in pipdata.items():
+            src, dst = src_dst
+            if pip in ignpip:
+                pass
+            elif pip in pips:
+                segmk.add_tile_tag(tile, "%s.%s" % (dst, src), 1)
+            elif src_dst[1] not in dsts:
+                segmk.add_tile_tag(tile, "%s.%s" % (dst, src), 0)
+
+    segmk.compile()
+    segmk.write()
